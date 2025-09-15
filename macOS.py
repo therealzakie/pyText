aee306fb3aa7c581ffb683bac8980b9ae9768152
therealzakie <therealzakie@zakie-computer.com>
2025-09-15 17:04:27 +0100

    change version num

--- macOS.py
+++ macOS.py
@@ -66,11 +66,7 @@ tab13 = "             "
 tab14 = "              "
 tab15 = "               "
 
-<<<<<<< HEAD
-pyText_version = "1.0.0 customtkinter build (develop branch)"
-=======
-pyText_version = "1.0.0 customtkinter build (master branch)"
->>>>>>> master
+pyText_version = "1.0.0 customtkinter build"
 
 if theme == "dark":
     root._set_appearance_mode("dark")
